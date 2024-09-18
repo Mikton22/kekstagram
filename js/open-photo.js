387436e8c0e8a1e8fbdb35ce9c432cee0c865836
Mikhail <mike144513@gmail.com>
2024-09-18 22:22:31 +0500

    upd open-photo.js

--- js/open-photo.js
+++ js/open-photo.js
@@ -1,35 +1,5 @@
 const bigPicture = document.querySelector('.big-picture');
-// const pictureImg = bigPicture.querySelector('.big-picture__img img');
-// const likesCount = bigPicture.querySelector('.likes-count');
-// const commentsCount = bigPicture.querySelector('.comments-count');
-// const socialComments = bigPicture.querySelector('.social__comments');
-// const socialCaption = bigPicture.querySelector('.social__caption');
-// const commentCountBlock = bigPicture.querySelector('.social__comment-count');
-// const commentsLoader = bigPicture.querySelector('.comments-loader');
 const closeButton = bigPicture.querySelector('.big-picture__cancel');
-
-// function renderComments(comments) {
-//   socialComments.innerHTML = '';
-//   comments.forEach ((comment) => {
-//     const commentElement = document.createElement('li');
-//     commentElement.classList.add('.social__comment');
-//
-//     const avatar = document.createElement('img');
-//     avatar.classList.add('.social_picture');
-//     avatar.src = comment.avatar;
-//     avatar.alt = comment.name;
-//     avatar.width = 35;
-//     avatar.height = 35;
-//
-//     const text = document.createElement('p');
-//     text.classList.add('social__text');
-//     text.textContent = comment.message;
-//
-//     commentElement.appendChild(avatar);
-//     commentElement.appendChild(text);
-//     socialComments.appendChild(commentElement);
-//   });
-// }
 
 export const openPicture = (miniature) => {
   document.querySelector('body').classList.add('modal-open');
